bf907f8fca02634cc0273216832fa407a3103eff
Johannes K <kirscjo@gmail.com>
2017-12-08 23:32:41 +0100

    Add example for approx equal test.

--- test_dvr.py
+++ test_dvr.py
@@ -1,8 +1,11 @@
+from pytest import approx
+
 import dvr
 
 
 def test_calc_mpotential_non_diag():
     assert dvr.calc_mpotential([1,2,3], [4,5,6], 3, 1, 1, 20) == 0.0
+
 
 def test_calc_mpotential_diag():
     # trivial case should evaluate to lec
@@ -13,3 +16,12 @@ def test_calc_mpotential_diag():
                                1,             # lec
                                20             # beta (gaussian width)
                               ) == 1.0
+
+    # TODO: write additonal test cases
+    # result = dvr.calc_mpotential([1, 1, 1, 1],  # row_idx
+    #                              [1, 1, 1, 1],  # col_idx
+    #                              2,             # space_dims
+    #                              1,             # grid spacing
+    #                              1,             # lec
+    #                              20             # beta (gaussian width)
+    #                             ) == approx(2.3)
