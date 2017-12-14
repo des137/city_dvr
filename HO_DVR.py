ad39c1e78a4e8d017782684466fd5a1d36dfd7bf
Johannes K <kirscjo@gmail.com>
2017-12-14 11:35:17 -0500

    1D HO DVR basis

--- HO_DVR.py
+++ HO_DVR.py
@@ -10,15 +10,15 @@ references are made to Phys. Rept. 324 (2000) 1-105
 
 """
 
-MASS = NUCLEON_MASS
-OMEGA = 2.
-XEQ = 0.
+MASS = NUCLEON_MASS / HBARC  # mass in [fm^-1]
+OMEGA = 1.9  # frequency in [fm]
+XEQ = 0.  # oscillator origin in [fm]
 
-GRID_DIM = 4
+GRID_DIM = 5
 pos_op_mat = np.zeros((GRID_DIM, GRID_DIM))
 
 
-# def HO basis functions (Eq.B31)
+# def HO basis functions (Eq.B31), pass arguments in consistent units.
 def phi(j, x, xeq, mass, omega):
     return 1. / np.sqrt(2**j * factorial(j)) * (
         mass * omega)**0.25 * hermite(j)(np.sqrt(mass * omega) * (
@@ -38,7 +38,6 @@ for j in range(GRID_DIM):
 # calculate U^+ X U , to convince yourself that you understand the syntax
 eigen_sys = np.linalg.eigh(pos_op_mat)
 eigen_pos = eigen_sys[0]
-print(np.dot(np.transpose(eigen_sys[1]), np.dot(pos_op_mat, eigen_sys[1])))
 
 # calculate the matrix elements of the second-derivative operator via Eq. B.36
 pot_op_mat = np.zeros((GRID_DIM, GRID_DIM))
@@ -46,20 +45,21 @@ kin_op_mat = np.zeros((GRID_DIM, GRID_DIM))
 for alpha in range(GRID_DIM):
     for beta in range(GRID_DIM):
         for k in range(GRID_DIM):
-            if alpha == beta:
-                pot_op_mat[alpha, beta] = (eigen_sys[0][alpha] - XEQ)**2
-
-            kin_op_mat[alpha, beta] += eigen_sys[1][k, alpha] * (
+            kin_op_mat[alpha, beta] += OMEGA * eigen_sys[1][k, alpha] * (
                 k + 0.5) * eigen_sys[1][k, beta]
-pot_op_mat *= (MASS * OMEGA)**2
-kin_op_mat *= (-2 * MASS * OMEGA)
-kin_op_mat = kin_op_mat + pot_op_mat
-pot_op_mat *= 1. / (2 * MASS)
+        if alpha == beta:
+            kin_op_mat[alpha, beta] -= 0.5 * MASS * OMEGA**2 * (
+                eigen_sys[0][alpha] - XEQ)**2
+            # here, a different interaction could be considered; only for this
+            # reason, this awkward construct instead of just H = omega \sum_k=1\toN-1 U^*_ka (k+1/2) U_kb is used
+            pot_op_mat[alpha, beta] = 0.5 * MASS * OMEGA**2 * (
+                eigen_sys[0][alpha] - XEQ)**2
+
 hamiltonian_mat = kin_op_mat + pot_op_mat
 eigen_sys = np.linalg.eigh(hamiltonian_mat)
-print(eigen_sys[0])
+print(eigen_sys[0][:5])
 nmax = 20
-print(eigenvalues_harmonic_osci(MASS * OMEGA**2, MASS, nmax)[:5])
+print(eigenvalues_harmonic_osci(OMEGA, nmax, 1)[:5])
 exit()
 # visualize a basis function along with the eigenvalues of the position operator
 test_grid = np.linspace(-0.2, 0.2, 100)
