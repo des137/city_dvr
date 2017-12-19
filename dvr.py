calculation of Ekin in the SINE basis corrected; consistency check included which fails if the eigenvalues of the position operator in the SINE rep. are not equally spaced;

dvr.py
@@ -33,7 +33,7 @@ def calc_grid(dimension, basis_specs):
         evsys = np.linalg.eigh(pos_op_mat)
         evs = basis_specs[1][2] + basis_specs[1][1] / np.pi * np.arccos(
             evsys[0])
-        return [evs, evsys[1]]
+        return [np.sort(evs), evsys[1]]
 
     if basis_specs[0] == 'HO':
         for j in range(dimension):
@@ -73,6 +73,16 @@ def calc_Ekin(dim_basis, n_part, basis_specs, eigensys_coord):
         # the eigenvalues; as the eigenvalues do not include the edges, 2*dx
         # has to be added;
 
+        if sum(
+                np.isclose(
+                    np.diff(eigensys_coord[0]),
+                    np.roll(np.diff(eigensys_coord[0]),
+                            1))) != len(eigensys_coord[0]) - 1:
+            print(
+                'Grid points of the SINE basis are not equally spaced! Exiting...'
+            )
+            exit()
+
         dx = abs(eigensys_coord[0][1] - eigensys_coord[0][0])
         L = abs(eigensys_coord[0][-1] - eigensys_coord[0][0]) + 2 * dx
         for i in range(dim_basis):
@@ -91,7 +101,7 @@ def calc_Ekin(dim_basis, n_part, basis_specs, eigensys_coord):
                                       (i_idx + ip_idx))**(-2))
                 tmp[i, ip] *= np.pi**2 / (2. * L**2)
 
-        tmp = tmp / (-2 * basis_specs[1][3])
+        tmp = tmp / (2. * basis_specs[1][3])
         # this factor has to be verified!
 
     if basis_specs[0] == 'HO':

dvr_main.py
@@ -22,12 +22,12 @@ X_EQ = 0.
 POT_HO = ['HO', X_EQ, MASS / HBARC, OMEGA]
 
 # lattice set-up
-PARTNBR = 2  # number of particles
+PARTNBR = 1  # number of particles
 SPACEDIMS = 2  # spatial coordinate dimensions (e.g. Cartesian x,y,z)
-BASIS_DIM = 5  # (dim of variational basis) = (nbr of grid points) = (nbr of segments - 1)
+BASIS_DIM = 55  # (dim of variational basis) = (nbr of grid points) = (nbr of segments - 1)
 
 # specify the variational basis
-BOX_SIZE = 6  # physical length of one spatial dimension (in Fermi); relevant for specific bases, only!
+BOX_SIZE = 8  #BASIS_DIM + 0  # physical length of one spatial dimension (in Fermi); relevant for specific bases, only!
 BOX_ORIGIN = -BOX_SIZE / 2.
 BASIS_SINE = ['SINE', [SPACEDIMS, BOX_SIZE, BOX_ORIGIN, MASS / HBARC]]
 BASIS_HO = ['HO', [SPACEDIMS, X_EQ, (MASS / HBARC), OMEGA]]
@@ -36,24 +36,23 @@ BASIS_HO = ['HO', [SPACEDIMS, X_EQ, (MASS / HBARC), OMEGA]]
 N_EIGENV = 14  # number of eigenvalues to be calculated with <eigsh>
 
 
-def calc_mhamilton(n_particle, dim_space, dim_basis, specs_basis,
-                   specs_potential):
+def calc_mhamilton(n_part, dim_space, dim_bas, spec_bas, spec_pot):
     """ Function returns the Hamilton matrix; 
 
-        :n_particle: number of particles
+        :n_part: number of particles
         :dim_space: spatial (Cartesian) dimensions
-        :dim_basis: variational-basis dim = number of segments each coordinate is divided into
-        :specs_potential: parameters specifying the interaction potential
-        :specs_basis: parameters specifying the basis
+        :dim_bas: variational-basis dim = number of segments each coordinate is divided into
+        :spec_pot: parameters specifying the interaction potential
+        :spec_bas: parameters specifying the basis
 
         :return: full Hamilton matrix in D(iscrete) V(ariable) R(epresentation)
     """
     # dimension of a single coordinate point; e.g., 2D 2Part: (x1,y1,x2,y2)
     # D spatial dimensions for each of the N particles;
-    dim_grdpoint = n_particle * dim_space
-    # each component of a grid point takes dim_basis discrete values
-    # e.g. x1 \in {x_1,...,x_dim_basis} where x_1 is an eigenvalue of the position matrix
-    dim_h = dim_basis**dim_grdpoint
+    dim_grdpoint = n_part * dim_space
+    # each component of a grid point takes dim_bas discrete values
+    # e.g. x1 \in {x_1,...,x_dim_bas} where x_1 is an eigenvalue of the position matrix
+    dim_h = dim_bas**dim_grdpoint
 
     # initialize empty matrices (might have to be "sparsed" for larger dim. problems)
     mpotential = np.zeros((dim_h, dim_h))
@@ -63,14 +62,14 @@ def calc_mhamilton(n_particle, dim_space, dim_basis, specs_basis,
     # obtain eigensystem of the position operator in the basis of choice
     # eigenvalues '=' grid points ; transformation matrix necessary for Ekin
     # STATUS: one basis for all coordinates (future: xy->HO, z->SINE)
-    coordOP_evSYS = calc_grid(dim_basis, specs_basis)
+    coordOP_evSYS = calc_grid(dim_bas, spec_bas)
 
     # calculate potential and kinetic-energy matrices for a chosen basis
     # STATUS: for each additional basis, the matrices need to be specified in this function!
-    mkinetic = calc_Ekin(dim_basis, n_particle, specs_basis, coordOP_evSYS)
+    mkinetic = calc_Ekin(dim_bas, n_part, spec_bas, coordOP_evSYS)
     # STATUS: the potential matrix is assumed to be diagonal (future: OPE+B => potential has non-zero offdiagonal elements)
     mpotential = np.diag(
-        calc_potential(n_particle, dim_space, specs_potential, coordOP_evSYS))
+        calc_potential(n_part, dim_space, spec_pot, coordOP_evSYS))
 
     mhamilton = (mkinetic + mpotential)
     return mhamilton
@@ -84,9 +83,9 @@ def calc_mhamilton(n_particle, dim_space, dim_basis, specs_basis,
 
 ham = []
 with benchmark("Matrix filling"):
-    ham = calc_mhamilton(PARTNBR, SPACEDIMS, BASIS_DIM, BASIS_HO, POT_HO)
+    ham = calc_mhamilton(PARTNBR, SPACEDIMS, BASIS_DIM, BASIS_SINE, POT_HO)
 
-sparsimonius = False  # False '=' full matrix diagonalization; True '=' approximate determination of the lowest <N_EIGEN> eigenvalues
+sparsimonius = True  # False '=' full matrix diagonalization; True '=' approximate determination of the lowest <N_EIGEN> eigenvalues
 
 if sparsimonius:
     with benchmark("Diagonalization -- sparse matrix structure (DVR)"):